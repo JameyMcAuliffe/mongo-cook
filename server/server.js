a83b29670fb1178137d87713a72404a6aa63416e
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2017-06-28 12:54:15 -0500

    html renders through express app

--- server/server.js
+++ server/server.js
@@ -4,16 +4,20 @@ let express = require('express')
 let app = express()
 let bodyParser = require('body-parser')
 let mongoose = require('mongoose')
-app.use(bodyParser.json())
-
 let Recipes = require('./models/recipes')
+let path = require('path')
 
 //Connect to mongoose
 mongoose.connect('mongodb://localhost:27017/Cookbook')
 let db = mongoose.connection
 
+//Express Middlewares
+app.use(express.static('client'))
+app.use(bodyParser.json())
+//app.set('view engine', 'html');
+
 app.get('/', function(req, res) {
-	res.send('Cookbook')
+	res.sendFile('home.html', { root: path.join(__dirname, '../client/partials') } )
 })
 
 app.get('/recipes', function(req, res) {
